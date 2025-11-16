c4a76268811d76177ffac4d14fa69171bf6db5d1
MahmoudAshraf <mahmoud@ArchLinux.com>
2025-11-16 07:09:18 +0200

    Added sorting functionality and updated login tests

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -10,86 +10,8 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class Login {
-    WebDriver driver;
+public class Login extends BaseClass{
 
-
-    //some web elements
-    WebElement username(){
-        return driver.findElement(By.id("user-name"));
-    }
-    WebElement password(){
-        return driver.findElement(By.id("password"));
-    }
-    WebElement LoginBtn(){
-        return driver.findElement(By.id("login-button"));
-    }
-
-
-
-    WebElement item_1_addToCart_Btn() { return driver.findElement(By.id("add-to-cart-sauce-labs-backpack")); }
-    WebElement item_2_addToCart_Btn(){
-        return driver.findElement(By.id("add-to-cart-sauce-labs-bike-light"));
-    }
-    WebElement item_3_addToCart_Btn(){
-        return driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt"));
-    }
-    WebElement item_4_addToCart_Btn(){
-        return driver.findElement(By.id("add-to-cart-sauce-labs-fleece-jacket"));
-    }
-    WebElement item_5_addToCart_Btn(){
-        return driver.findElement(By.id("add-to-cart-sauce-labs-onesie"));
-    }
-    WebElement item_6_addToCart_Btn() { return driver.findElement(By.id("add-to-cart-test.allthethings()-t-shirt-(red)")); }
-
-
-    WebElement item_1_Remove_Btn() { return driver.findElement(By.id("remove-sauce-labs-backpack")); }
-    WebElement item_2_Remove_Btn() { return driver.findElement(By.id("remove-sauce-labs-bike-light")); }
-    WebElement item_3_Remove_Btn() { return driver.findElement(By.id("remove-sauce-labs-bolt-t-shirt")); }
-    WebElement item_4_Remove_Btn() { return driver.findElement(By.id("remove-sauce-labs-fleece-jacket")); }
-    WebElement item_5_Remove_Btn() { return driver.findElement(By.id("remove-sauce-labs-onesie")); }
-    WebElement item_6_Remove_Btn() { return driver.findElement(By.id("remove-test.allthethings()-t-shirt-(red)")); }
-
-    WebElement sortAtoZ() { return driver.findElement(By.cssSelector("option[value='az']"));}
-    WebElement sortZtoA() { return driver.findElement(By.cssSelector("option[value='za']"));}
-    WebElement sortHightoLow() { return driver.findElement(By.cssSelector("option[value='hilo']"));}
-    WebElement sortLowtoHigh() { return driver.findElement(By.cssSelector("option[value='lohi']"));}
-
-    WebElement shopping_Cart_Btn(){ return driver.findElement(By.cssSelector("a.shopping_cart_link"));}
-    WebElement checkOut_Btn(){ return driver.findElement(By.id("checkout"));}
-
-    WebElement firstname_Input(){return driver.findElement(By.id("first-name"));}
-    WebElement lastname_Input(){return driver.findElement(By.id("last-name"));}
-    WebElement zip_Code_Input(){return driver.findElement(By.id("postal-code"));}
-
-    WebElement continue_CheckOut_Btn(){ return driver.findElement(By.id("continue"));}
-
-    WebElement finish_Btn(){ return driver.findElement(By.id("finish"));}
-    WebElement complete_Header(){ return driver.findElement(By.cssSelector("h2.complete-header"));}
-    WebElement back_Home_Btn(){ return driver.findElement(By.id("back-to-products"));}
-
-    WebElement logout_Btn(){ return driver.findElement(By.id("logout_sidebar_link"));}
-
-    WebElement menu_Btn(){ return driver.findElement(By.id("react-burger-menu-btn"));}
-
-
-
-
-
-
-
-
-    @BeforeTest
-    public void prepare() {
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/");
-    }
-
-    @AfterTest
-    public void teardown(){
-        driver.quit();
-    }
 
     @Test
     public void standerUser(){
@@ -97,10 +19,7 @@ public class Login {
         password().sendKeys("secret_sauce");
         LoginBtn().click();
 
-//        System.out.println(driver.getCurrentUrl().equals("www."));
         Assert.assertEquals(driver.getCurrentUrl(),"https://www.saucedemo.com/inventory.html");
-//        Assert.assertTrue(driver.getTitle().equals("https://www.saucedemo.com/inventory.html"),"");
-//        Assert.assertTrue(driver.getCurrentUrl().equals("https://www.saucedemo.com/inventory.html"),"");
     }
 
 
--- src/test/java/Sorting.java
+++ src/test/java/Sorting.java
@@ -1,2 +1,67 @@
-public class Sorting {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Sorting extends LoginTest {
+    @Test(priority = 5)
+    public void sortingByNameAtoZ() {
+        testValidLogin();
+        List<String> Items_names_Before_Sort = getItemNames();
+        sortAtoZ().click();
+        List<String> Items_names_After_Sort = getItemNames();
+
+        Collections.sort(Items_names_Before_Sort);
+        Assert.assertEquals(sortAtoZ().getText(), "Name (A to Z)");
+        Assert.assertEquals(Items_names_Before_Sort, Items_names_After_Sort);
+    }
+
+    @Test(priority = 6)
+    public void sortingByNameZtoA() {
+        testValidLogin();
+        List<String> Items_names_Before_Sort = getItemNames();
+        sortZtoA().click();
+        List<String> Items_names_After_Sort = getItemNames();
+
+        Collections.sort(Items_names_Before_Sort, Collections.reverseOrder());
+        Assert.assertEquals(sortZtoA().getText(), "Name (Z to A)");
+        Assert.assertEquals(Items_names_Before_Sort, Items_names_After_Sort);
+    }
+
+    @Test(priority = 7)
+    public void sortingByPriceLowtoHigh() {
+        testValidLogin();
+        List<Double> Items_prices_Before_Sort = getItemPrices();
+        Items_prices_Before_Sort.forEach(e -> System.out.println("Before: " + e));
+        sortLowtoHigh().click();
+        List<Double> Items_prices_After_Sort = getItemPrices();
+
+        Collections.sort(Items_prices_Before_Sort);
+        Items_prices_Before_Sort.forEach(e -> System.out.println("Before2: " + e));
+        Items_prices_After_Sort.forEach(e -> System.out.println("After: " + e));
+        Assert.assertEquals(sortLowtoHigh().getText(), "Price (low to high)");
+        Assert.assertEquals(Items_prices_Before_Sort, Items_prices_After_Sort);
+
+    }
+
+    @Test(priority = 8)
+    public void sortingByPriceHightoLow() {
+        testValidLogin();
+        List<Double> Items_prices_Before_Sort = getItemPrices();
+        Items_prices_Before_Sort.forEach(e -> System.out.println("Before: " + e));
+        sortHightoLow().click();
+        List<Double> Items_prices_After_Sort = getItemPrices();
+
+        Collections.sort(Items_prices_Before_Sort, Collections.reverseOrder());
+        Items_prices_After_Sort.forEach(e -> System.out.println("After: " + e));
+        Assert.assertEquals(sortHightoLow().getText(), "Price (high to low)");
+        Assert.assertEquals(Items_prices_Before_Sort, Items_prices_After_Sort);
+
+    }
 }
